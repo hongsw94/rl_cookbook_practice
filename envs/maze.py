388f127e70140305cd972ef0ac7d3a7f5a245814
Sungweon (Sam) Hong <hongsw94@hanyang.ac.kr>
2022-02-11 19:36:45 +0900

    - - - -

---
+++ envs/maze.py
@@ -0,0 +1,29 @@
+# Maze RL environment with image observaitons
+
+from typing import List 
+
+import gym
+import numpy as np
+
+class MaxeEnv(gym.Env):
+    def __init__(self):
+        pass 
+
+    def reset(self):
+        pass 
+
+    def set_state(self,):
+        pass 
+
+    def step(self):
+        pass
+
+    def num2coin(self):
+        pass 
+
+    def coin2num(self):
+        pass 
+
+    def render(self):
+        pass 
+    
