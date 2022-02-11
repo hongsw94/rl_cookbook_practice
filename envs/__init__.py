a4ce698409f88e7b9037ffc0ae3af852447df08a
Sungweon (Sam) Hong <hongsw94@hanyang.ac.kr>
2022-02-11 19:30:38 +0900

    First commit -'environments'

---
+++ envs/__init__.py
@@ -0,0 +1,9 @@
+from gym.envs.registration import register 
+
+register(
+    id="Maze-v0", entry_point="envs.maze: MazeEnv",
+)
+
+register(
+    id="Gridworld-v2", entry_point="envs.gridworldv2:GridworldV2Env"
+)
